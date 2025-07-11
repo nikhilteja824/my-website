fd07380a657579727558f8cc6e1055c225428372
Nikhil Teja Malyala <nikhilteja85@gmail.com>
2025-07-11 02:13:44 -0400

    Updated contact page

--- src/lib/certifications.ts
+++ src/lib/certifications.ts
@@ -1,10 +1,14 @@
 export const certifications = [
     {
+      name: "Docker Essential Training",
+      link: "https://www.linkedin.com/learning/certificates/4466fc8b60f81a8f0f656d1930a550be579a21a6e2a3fe1a65c51373d989cc14?trk=share_certificate",
+    },
+    {
       name: "AWS Certified Solutions Architect – Associate",
       link: "https://www.credly.com/",
     },
     {
-      name: "Google Cloud Associate Engineer",
-      link: "https://cloud.google.com/certification",
+      name: "Object Oriented Programming in Java by UC San Diego",
+      link: "https://coursera.org/share/a86897d2b43e3cbda827653550fe6651",
     },
   ];
